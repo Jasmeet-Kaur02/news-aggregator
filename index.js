72b5de45107c93427aafc9ad2c76704ddbb7c19f
JasmeetKaurx <jasskaur.dha@gmail.com>
2023-10-15 18:57:48 +0530

    Revert "created signup API"
    This reverts commit c18e36acfbcdd894b23c8f1eca062eee33cfd984.

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 const express = require("express");
-const userNewsRoutes = require("./src/router/UserNews");
-const { signup, signin } = require("./src/controllers/AuthController");
 
 const PORT = 3000;
-const app = express();
-
-app.use(express.json());
+app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome to the News Aggregator");
 });
-
-app.post("/signup", signup);
-
-app.post("/signin", signin);
 
 app.listen(PORT, (err) => {
   if (err) {
