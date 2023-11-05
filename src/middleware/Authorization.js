ec4b82b4c1f7f3dbecadc59af639e9aa11bb3422
JasmeetKaurx <jasskaur.dha@gmail.com>
2023-11-05 10:02:03 +0530

    fixed users and authorization middleware

--- src/middleware/Authorization.js
+++ src/middleware/Authorization.js
@@ -2,10 +2,11 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
   if (req.headers && req.headers.authorization) {
-    const token = req.headers.authorization.split(" ")[1];
+    const token =
+      req.headers.authorization.split(" ")[1] ?? req.headers.authorization;
     jwt.verify(token, process.env.API_KEY, (authError) => {
       if (authError) {
-        return res.status(500).json({
+        return res.status(401).json({
           status: false,
           message: "Account is unauthorized.",
           data: null,
--- src/middleware/users.js
+++ src/middleware/users.js
@@ -1,18 +1,30 @@
-const users = require("../data/users.json");
+const fs = require("fs/promises");
+const path = require("path");
 
 const validateUserId = (req, res, next) => {
-  const parsedUsers = JSON.parse(JSON.stringify(users));
-  const userId = req.params.id;
-  if (parsedUsers.some((user) => user.id === userId)) {
-    req.id = userId;
-    next();
-  } else {
-    return res.status(404).json({
-      status: false,
-      message: "User with the provided id doesn't not exists.",
-      data: null,
+  const filePath = path.join(__dirname, "..", "data", "users.json");
+  fs.readFile(filePath, { encoding: "utf-8" })
+    .then((users) => {
+      const parsedUsers = JSON.parse(users);
+      const userId = req.params.id;
+      if (parsedUsers.some((user) => user.id === userId)) {
+        req.id = userId;
+        next();
+      } else {
+        return res.status(404).json({
+          status: false,
+          message: "User with the provided id doesn't not exists.",
+          data: null,
+        });
+      }
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        status: false,
+        message: "Internal Server Error",
+        data: null,
+      });
     });
-  }
 };
 
 module.exports = { validateUserId };
